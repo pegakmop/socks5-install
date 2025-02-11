2c69270addf345c27084f9ba801b9a1708261258
pegakmop <93389368+pegakmop@users.noreply.github.com>
2025-02-11 23:56:42 +0300

    Create socks5.sh

---
+++ socks5.sh
@@ -0,0 +1,219 @@
+#!/bin/bash
+
+SOCKS5_INSTALL_PATH="/usr/local"
+
+CHECK_OS(){
+	if [[ -f /etc/redhat-release ]];then
+		release="centos"
+	elif cat /etc/issue | grep -q -E -i "debian";then
+		release="debian"
+	elif cat /etc/issue | grep -q -E -i "ubuntu";then
+		release="ubuntu"
+	elif cat /etc/issue | grep -q -E -i "centos|red hat|redhat";then
+		release="centos"
+	elif cat /proc/version | grep -q -E -i "debian";then
+		release="debian"
+	elif cat /proc/version | grep -q -E -i "ubuntu";then
+		release="ubuntu"
+	elif cat /proc/version | grep -q -E -i "centos|red hat|redhat";then
+		release="centos"
+	fi
+}
+
+CLOSE_THE_FIREWALL(){
+	CLOSE_THE_IPTABLES(){
+		iptables -F
+		iptables -X
+		iptables -I INPUT -p tcp -m tcp --dport 1:65535 -j ACCEPT
+		iptables -I INPUT -p udp -m udp --dport 1:65535 -j ACCEPT
+		iptables-save > /etc/sysconfig/iptables
+		echo 'iptables-restore /etc/sysconfig/iptables' >> /etc/rc.local
+	}
+	CLOSE_THE_FIREWALLD(){
+		if [ -e /usr/bin/systemctl ];then
+			systemctl stop firewalld.service
+			systemctl disable firewalld.service
+		fi
+	}
+	
+	CLOSE_THE_IPTABLES
+	CLOSE_THE_FIREWALLD
+}
+
+SHOW_SOCKS5(){
+	clear
+	echo "
+ ######   #######   ######  ##    ##  ######  ######## 
+##    ## ##     ## ##    ## ##   ##  ##    ## ##       
+##       ##     ## ##       ##  ##   ##       ##       
+ ######  ##     ## ##       #####     ######  #######  
+      ## ##     ## ##       ##  ##         ##       ## 
+##    ## ##     ## ##    ## ##   ##  ##    ## ##    ## 
+ ######   #######   ######  ##    ##  ######   ######  
+"
+}
+
+INSTALL_SOCKS5(){
+	if [ -e /etc/opt/ss5/ss5.conf ];then
+		echo "SOCKS5 уже установлен."
+		exit
+	fi
+	
+	CHECK_OS
+	case "${release}" in
+		centos)
+			yum -y install iptables firewalld iptables-services
+			yum -y install gcc zip git curl wget unzip screen net-tools pam-devel openssl-devel openldap-devel;;
+		debian|ubuntu)
+			echo "Извините, системы, отличные от CentOS, не поддерживаются."
+			exit;;
+	esac
+	
+	# Скачать и скомпилировать
+	wget -P ${SOCKS5_INSTALL_PATH} "http://downloads.sourceforge.net/project/ss5/ss5/3.8.9-8/ss5-3.8.9-8.tar.gz"
+	cd ${SOCKS5_INSTALL_PATH}
+	tar -xzvf ss5-3.8.9-8.tar.gz
+	rm -rf ss5-3.8.9-8.tar.gz
+	cd ss5-3.8.9
+	./configure
+	make
+	make install
+	cd /root
+	# Включить аутентификацию
+	sed -i '87c auth    0.0.0.0/0               -              u' /etc/opt/ss5/ss5.conf
+	sed -i '203c permit  u       0.0.0.0/0       -       0.0.0.0/0       -       -       -       -       -' /etc/opt/ss5/ss5.conf
+	rm -rf /etc/opt/ss5/ss5.passwd
+	# Исправить ошибку синтаксиса в /etc/rc.d/init.d/ss5
+	sed -i '18c  [[ ${NETWORKING} = "no" ]] && exit 0' /etc/rc.d/init.d/ss5
+	# Установить автозапуск
+	chmod u+x /etc/rc.d/init.d/ss5
+	chkconfig --add ss5
+	chkconfig ss5 on
+	service ss5 start
+	# Закрыть файрвол
+	CLOSE_THE_FIREWALL
+	
+	SHOW_SOCKS5
+	echo "Установка SOCKS5 завершена. Вам нужно добавить пользователя для его использования. Используйте команду socks5 user add для добавления."
+}
+
+UNINSTALL_SOCKS5(){
+	cd ${SOCKS5_INSTALL_PATH}/ss5-3.8.9
+	make uninstall
+	cd /root
+	rm -rf ${SOCKS5_INSTALL_PATH}/ss5-3.8.9
+	rm -rf /etc/opt/ss5/ss5.conf
+	
+	SHOW_SOCKS5
+	echo "SOCKS5 был удален."
+}
+
+ADD_USER(){
+	username=${1}
+	password=${2}
+	
+	if [[ ${username} = "" ]] || [[ ${password} = "" ]];then
+		echo "Вы должны указать как имя пользователя, так и пароль."
+		echo "Например, чтобы добавить пользователя 123 с паролем 456, используйте команду socks5 user add 123 456."
+		exit
+	else
+		echo "${username} ${password}" >> /etc/opt/ss5/ss5.passwd
+		service ss5 restart > /dev/null
+		
+		SHOW_SOCKS5
+		Address=$(curl -s ipv4.ip.sb)
+		Shadowrocket_Address=$(echo ${username}:${password}@${Address}:1080 | base64)
+		echo "Готово, UserName:${username} PassWord:${password} Address:${Address}"
+		echo
+		echo "Telegram : tg://socks?server=${Address}&port=1080&user=${username}&pass=${password}"
+		echo "Shadowrocket : socks://${Shadowrocket_Address}"
+	fi
+}
+
+DEL_USER(){
+	echo "Список пользователей:"
+	echo;cat -n /etc/opt/ss5/ss5.passwd;echo
+	
+	read -p "Введите ID пользователя для удаления:" DELETE_USER_ID
+		if [[ ${DELETE_USER_ID} = "" ]];then
+			echo "ID пользователя для удаления не может быть пустым."
+			exit
+		else
+			sed -i "${DELETE_USER_ID}d" /etc/opt/ss5/ss5.passwd
+			service ss5 restart > /dev/null
+		fi
+		
+	SHOW_SOCKS5
+	echo "Завершено, пользователь был удален."
+}
+
+INSTALL_CHECK(){
+	if [ ! -e /usr/bin/socks5 ];then
+		mv /root/socks5.sh /usr/bin/socks5
+		chmod 755 /usr/bin/socks5
+		socks5
+	fi
+}
+
+command_1=${1}
+command_2=${2}
+command_3=${3}
+command_4=${4}
+INSTALL_CHECK
+
+if [ -e /etc/opt/ss5/ss5.conf ];then
+	case "${command_1}" in
+		install)
+			INSTALL_SOCKS5;;
+		uninstall)
+			UNINSTALL_SOCKS5;;
+		user)
+			clear
+			case "${command_2}" in
+				add)
+					ADD_USER ${command_3} ${command_4};;
+				del)
+					DEL_USER;;
+				list)
+					clear;cat -n /etc/opt/ss5/ss5.passwd;;
+				*)
+					echo "socks5 user {del|list}"
+					echo "socks5 user add \$username \$password";;
+			esac;;
+		start)
+			service ss5 start;;
+		stop)
+			service ss5 stop;;
+		restart)
+			service ss5 restart;;
+		status)
+			service ss5 status;;
+		update)
+			rm -rf /usr/bin/socks5
+			wget -qO /usr/bin/socks5 "https://raw.githubusercontent.com/qinghuas/socks5-install/master/socks5.sh"
+			chmod 755 /usr/bin/socks5
+			echo "Обновление завершено.";;
+		info)
+			clear
+			SHOW_SOCKS5
+			echo "GitHub : https://github.com/qinghuas/socks5-install"
+			echo "By : @qinghuas V.2.0.3 2018-03-25";;
+		*)
+			SHOW_SOCKS5
+			echo "socks5 {update|info}"
+			echo "socks5 user {add|del|list}"
+			echo "socks5 {install|uninstall}"
+			echo "socks5 {start|stop|restart|status}";;
+	esac
+else
+	case "${command_1}" in
+		install)
+			INSTALL_SOCKS5;;
+		*)
+			SHOW_SOCKS5
+			echo "Сначала установите socks5 для выполнения других операций. Используйте команду socks5 install для установки."
+			exit;;
+	esac
+fi
+
+# END
